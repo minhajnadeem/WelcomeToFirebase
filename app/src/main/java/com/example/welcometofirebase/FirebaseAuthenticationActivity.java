a94480bfb4f6c33d6a9c3a728bde110dfa2dc729
MinhajNadeem <minhaj.lib@gmail.com>
2019-01-25 23:15:28 +0500

    Update FirebaseAuthenticationActivity.java

--- app/src/main/java/com/example/welcometofirebase/FirebaseAuthenticationActivity.java
+++ app/src/main/java/com/example/welcometofirebase/FirebaseAuthenticationActivity.java
@@ -44,7 +44,8 @@ public class FirebaseAuthenticationActivity extends AppCompatActivity {
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
 
 // Create and launch sign-in intent
         startActivityForResult(
